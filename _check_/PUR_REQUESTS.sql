4d708d48e5b4cf0b81675f88d379b8403fdaed4e
Hafez Rabbani <hafezrabbani@gmail.com>
2024-05-08 08:28:40 +0330

    14030218

---
+++ _check_/PUR_REQUESTS.sql
@@ -0,0 +1,158 @@
+SELECT R.NUM_PGV_PURRQ
+      ,CASE
+         WHEN EXISTS (SELECT NULL
+                 FROM PUR.PUR_REQUEST_POSITIONS RP
+                INNER JOIN PUR.PUR_REQUEST_REPLENISHMENTS RR
+                   ON RP.PURRP_ID = RR.PURRP_PURRP_ID
+                WHERE RP.PURRQ_NUM_PGV_PURRQ = R.NUM_PGV_PURRQ) THEN
+          'is on replenish'
+       END AS RPLNSH
+      ,R.CCNTR_COD_CC_CCNTR
+      ,R.MDELP_DELIVERY_POINTS_ID
+      ,R.DAT_STR_VALD_PURRQ
+      ,TO_CHAR(R.DAT_STR_VALD_PURRQ
+              ,'YYYY/MM/DD HH24:MI:SS'
+              ,'NLS_CALENDAR=PERSIAN') AS DAT_STR_VALD_PURRQ_H
+      ,R.PAYCN_COD_PYM_PAYCN
+      ,R.DES_SHO1_PURRQ
+      ,R.DES_SHO2_PURRQ
+      ,R.DAT_DOC_ISU_PURRQ
+      ,TO_CHAR(R.DAT_DOC_ISU_PURRQ
+              ,'YYYY/MM/DD HH24:MI:SS'
+              ,'NLS_CALENDAR=PERSIAN') AS DAT_DOC_ISU_PURRQ_H
+      ,R.DAT_END_VALD_PURRQ
+      ,TO_CHAR(R.DAT_END_VALD_PURRQ
+              ,'YYYY/MM/DD HH24:MI:SS'
+              ,'NLS_CALENDAR=PERSIAN') AS DAT_END_VALD_PURRQ_H
+      ,R.AMN_CUR_PURRQ
+      ,R.DAT_CUR_PGSS_PURRQ
+      ,TO_CHAR(R.DAT_CUR_PGSS_PURRQ
+              ,'YYYY/MM/DD HH24:MI:SS'
+              ,'NLS_CALENDAR=PERSIAN') AS DAT_CUR_PGSS_PURRQ_H
+      ,R.BUYER_COD_PUR_BUYER
+      ,R.TXT_USR_PURRQ
+      ,R.TOT_NUM_POS_PURRQ
+      ,R.TOT_SUS_DAYS_PURRQ
+      ,R.DAT_STR_SUS_PURRQ
+      ,TO_CHAR(R.DAT_STR_SUS_PURRQ
+              ,'YYYY/MM/DD HH24:MI:SS'
+              ,'NLS_CALENDAR=PERSIAN') AS DAT_STR_SUS_PURRQ_H
+      ,R.DAT_END_SUS_PURRQ
+      ,TO_CHAR(R.DAT_END_SUS_PURRQ
+              ,'YYYY/MM/DD HH24:MI:SS'
+              ,'NLS_CALENDAR=PERSIAN') AS DAT_END_SUS_PURRQ_H
+      ,R.DAT_ISU_INQ_PURRQ
+      ,TO_CHAR(R.DAT_ISU_INQ_PURRQ
+              ,'YYYY/MM/DD HH24:MI:SS'
+              ,'NLS_CALENDAR=PERSIAN') AS DAT_ISU_INQ_PURRQ_H
+      ,R.DAT_STR_INQ_PURRQ
+      ,TO_CHAR(R.DAT_STR_INQ_PURRQ
+              ,'YYYY/MM/DD HH24:MI:SS'
+              ,'NLS_CALENDAR=PERSIAN') AS DAT_STR_INQ_PURRQ_H
+      ,R.DAT_END_INQ_PURRQ
+      ,TO_CHAR(R.DAT_END_INQ_PURRQ
+              ,'YYYY/MM/DD HH24:MI:SS'
+              ,'NLS_CALENDAR=PERSIAN') AS DAT_END_INQ_PURRQ_H
+      ,R.DAT_STR_JOB_PURRQ
+      ,TO_CHAR(R.DAT_STR_JOB_PURRQ
+              ,'YYYY/MM/DD HH24:MI:SS'
+              ,'NLS_CALENDAR=PERSIAN') AS DAT_STR_JOB_PURRQ_H
+      ,R.DAT_END_JOB_PURRQ
+      ,TO_CHAR(R.DAT_END_JOB_PURRQ
+              ,'YYYY/MM/DD HH24:MI:SS'
+              ,'NLS_CALENDAR=PERSIAN') AS DAT_END_JOB_PURRQ_H
+      ,R.AMN_TRNS_PURRQ
+      ,R.LKP_TYP_DOC_PURRQ
+      ,APPS.APP_FND_LOOKUP_PKG.GET_FARSI_MEANING_FUN(UPPER('PUR_REQUESTS')
+                                                    ,UPPER('LKP_TYP_DOC_PURRQ')
+                                                    ,R.LKP_TYP_DOC_PURRQ) AS LKP_TYP_DOC_PURRQ_DES
+      ,R.LKP_TYP_INQ_PURRQ
+      ,APPS.APP_FND_LOOKUP_PKG.GET_FARSI_MEANING_FUN(UPPER('PUR_REQUESTS')
+                                                    ,UPPER('LKP_TYP_INQ_PURRQ')
+                                                    ,R.LKP_TYP_INQ_PURRQ) AS LKP_TYP_INQ_PURRQ_DES
+      ,R.LKP_TYP_DEBT_PURRQ
+      ,APPS.APP_FND_LOOKUP_PKG.GET_FARSI_MEANING_FUN(UPPER('PUR_REQUESTS')
+                                                    ,UPPER('LKP_TYP_DEBT_PURRQ')
+                                                    ,R.LKP_TYP_DEBT_PURRQ) AS LKP_TYP_DEBT_PURRQ_DES
+      ,R.LKP_TYP_DLV_PURRQ
+      ,APPS.APP_FND_LOOKUP_PKG.GET_FARSI_MEANING_FUN(UPPER('PUR_REQUESTS')
+                                                    ,UPPER('LKP_TYP_DLV_PURRQ')
+                                                    ,R.LKP_TYP_DLV_PURRQ) AS LKP_TYP_DLV_PURRQ_DES
+      ,R.LKP_TYP_REQ_PURRQ
+      ,APPS.APP_FND_LOOKUP_PKG.GET_FARSI_MEANING_FUN(UPPER('PUR_REQUESTS')
+                                                    ,UPPER('LKP_TYP_REQ_PURRQ')
+                                                    ,R.LKP_TYP_REQ_PURRQ) AS LKP_TYP_REQ_PURRQ_DES
+      ,R.LKP_COD_STEP_PURRQ
+      ,APPS.APP_FND_LOOKUP_PKG.GET_FARSI_MEANING_FUN(UPPER('PUR_REQUESTS')
+                                                    ,UPPER('LKP_COD_STEP_PURRQ')
+                                                    ,R.LKP_COD_STEP_PURRQ) AS LKP_COD_STEP_PURRQ_DES
+      ,R.LKP_COD_SUS_PURRQ
+      ,APPS.APP_FND_LOOKUP_PKG.GET_FARSI_MEANING_FUN(UPPER('PUR_REQUESTS')
+                                                    ,UPPER('LKP_COD_SUS_PURRQ')
+                                                    ,R.LKP_COD_SUS_PURRQ) AS LKP_COD_SUS_PURRQ_DES
+      ,R.LKP_TYP_CNT_PURRQ
+      ,APPS.APP_FND_LOOKUP_PKG.GET_FARSI_MEANING_FUN(UPPER('PUR_REQUESTS')
+                                                    ,UPPER('LKP_TYP_CNT_PURRQ')
+                                                    ,R.LKP_TYP_CNT_PURRQ) AS LKP_TYP_CNT_PURRQ_DES
+      ,R.LKP_COD_MAKE_BUYE_PURRQ
+      ,APPS.APP_FND_LOOKUP_PKG.GET_FARSI_MEANING_FUN(UPPER('PUR_REQUESTS')
+                                                    ,UPPER('LKP_COD_MAKE_BUYE_PURRQ')
+                                                    ,R.LKP_COD_MAKE_BUYE_PURRQ) AS LKP_COD_MAKE_BUYE_PURRQ_DES
+      ,R.CREATE_DATE
+      ,R.CREATE_BY_DB_USER
+      ,R.CREATE_BY_APP_USER
+      ,R.LAST_UPDATE_DATE
+      ,R.LAST_UPDATE_BY_DB_USER
+      ,R.LAST_UPDATE_BY_APP_USER
+      ,R.ATTACH_ID
+      ,R.FLG_UNDER_CNT_PURRQ
+      ,R.DAT_EXTEND_INQ_PURRQ
+      ,TO_CHAR(R.DAT_EXTEND_INQ_PURRQ
+              ,'YYYY/MM/DD HH24:MI:SS'
+              ,'NLS_CALENDAR=PERSIAN') AS DAT_EXTEND_INQ_PURRQ_H
+      ,R.DAT_DEBTS_APPROVED_PURRQ
+      ,TO_CHAR(R.DAT_DEBTS_APPROVED_PURRQ
+              ,'YYYY/MM/DD HH24:MI:SS'
+              ,'NLS_CALENDAR=PERSIAN') AS DAT_DEBTS_APPROVED_PURRQ_H
+      ,R.FLG_DEBTS_APPROVED_PURRQ
+      ,R.DES_REJECT_PURRQ
+      ,R.DAT_VALD_INQUIRY_PURRQ
+      ,TO_CHAR(R.DAT_VALD_INQUIRY_PURRQ
+              ,'YYYY/MM/DD HH24:MI:SS'
+              ,'NLS_CALENDAR=PERSIAN') AS DAT_VALD_INQUIRY_PURRQ_H
+      ,R.DES_INQ_PURRQ
+      ,R.LKP_COD_MONEY_PURRQ
+      ,APPS.APP_FND_LOOKUP_PKG.GET_FARSI_MEANING_FUN(UPPER('PUR_REQUESTS')
+                                                    ,UPPER('LKP_COD_MONEY_PURRQ')
+                                                    ,R.LKP_COD_MONEY_PURRQ) AS LKP_COD_MONEY_PURRQ_DES
+      ,R.LKP_TYP_DEAL_PURRQ
+      ,APPS.APP_FND_LOOKUP_PKG.GET_FARSI_MEANING_FUN(UPPER('PUR_REQUESTS')
+                                                    ,UPPER('LKP_TYP_DEAL_PURRQ')
+                                                    ,R.LKP_TYP_DEAL_PURRQ) AS LKP_TYP_DEAL_PURRQ_DES
+      ,R.FLG_OPEN_CONTRACT_PURRQ
+      ,R.DES_PUR_ADMIN_PURRQ
+      ,R.DES_CDEAL_PURRQ
+      ,R.NAM_SUGG_SUPP_PURRQ
+      ,R.FLG_CDL_PURRQ
+      ,R.LKP_LEVEL_BARGAIN_PURRQ
+      ,APPS.APP_FND_LOOKUP_PKG.GET_FARSI_MEANING_FUN(UPPER('PUR_REQUESTS')
+                                                    ,UPPER('LKP_LEVEL_BARGAIN_PURRQ')
+                                                    ,R.LKP_LEVEL_BARGAIN_PURRQ) AS LKP_LEVEL_BARGAIN_PURRQ_DES
+      ,R.LKP_COD_CLOSE_PURRQ
+      ,APPS.APP_FND_LOOKUP_PKG.GET_FARSI_MEANING_FUN(UPPER('PUR_REQUESTS')
+                                                    ,UPPER('LKP_COD_CLOSE_PURRQ')
+                                                    ,R.LKP_COD_CLOSE_PURRQ) AS LKP_COD_CLOSE_PURRQ_DES
+      ,R.LKP_PLACE_REQ_PURRQ
+      ,APPS.APP_FND_LOOKUP_PKG.GET_FARSI_MEANING_FUN(UPPER('PUR_REQUESTS')
+                                                    ,UPPER('LKP_PLACE_REQ_PURRQ')
+                                                    ,R.LKP_PLACE_REQ_PURRQ) AS LKP_PLACE_REQ_PURRQ_DES
+  FROM PUR.PUR_REQUESTS R
+  LEFT OUTER JOIN PUR.PUR_BUYERS A5
+    ON R.BUYER_COD_PUR_BUYER = A5.COD_PUR_BUYER
+  LEFT OUTER JOIN COA.COA_COST_CENTERS A1
+    ON R.CCNTR_COD_CC_CCNTR = A1.COD_CC_CCNTR
+  LEFT OUTER JOIN MAM.MAM_DELIVERY_POINTS DP
+    ON R.MDELP_DELIVERY_POINTS_ID = DP.DELIVERY_POINTS_ID
+--  LEFT OUTER JOIN PUA.PUA_PAYMENT_CONDITIONS A4    ON r.PAYCN_COD_PYM_PAYCN = A4.COD_PYM_PAYCN
+ WHERE 1 = 1
+--   AND R.NUM_PGV_PURRQ IN (&NUM_PGV_PURRQ)
